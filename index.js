a6137209f38596ff3fa53b3e8bdd1b0b850110c6
pedroresd1 <pedroresd1@gmail.com>
2022-03-14 19:21:37 -0300

    handlebars cadastro e lista de alunos

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const sequelize = require('sequelize');
+const { engine } = require('express-handlebars');
 const app = express();
 
+//Handlebars
+app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//Routers
 app.get('/', (req, res) => {
-    res.send('Bem vindo ao Cadastro de Alunos')
+    res.send('Bem vindo ao Cadastro de Alunos');
+});
+
+app.get('/cad-alunos', (req, res) => {
+    res.render("cad-alunos");
+});
+
+app.get('/list-alunos', (req, res) => {
+    res.render("list-alunos");
 });
 
 app.listen(8080);
